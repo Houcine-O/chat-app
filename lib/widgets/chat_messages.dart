03f83327c1b9cc6f5b7cc649478f57ad2604a355
Houcine <houcin.ourabah@gmail.com>
2024-01-21 12:15:49 +0100

    displaying the messages as Strem

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +6,37 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("No messages for now..."),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createdAt', descending: false)
+            .snapshots(),
+        builder: (context, chatSnap) {
+          if (chatSnap.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (!chatSnap.hasData || chatSnap.data!.docs.isEmpty) {
+            return const Center(
+              child: Text("No messages for now..."),
+            );
+          }
+
+          if (chatSnap.hasError) {
+            return const Center(
+              child: Text(
+                "Something went wrong...",
+                style: TextStyle(color: Colors.red),
+              ),
+            );
+          }
+          final loadedMsgs = chatSnap.data!.docs;
+          return ListView.builder(
+              itemCount: loadedMsgs.length,
+              itemBuilder: (context, index) {
+                return Text(
+                  loadedMsgs[index].data()['text'],
+                );
+              });
+        });
   }
 }
